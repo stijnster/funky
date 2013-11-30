94ce1a8a8bf5d512caa053003ca85606058269dc
Stijn Mathysen <stijn@skylight.be>
2013-11-30 15:17:10 +0100

    extract common functionality from the boot file and clean up the framework file to a real class with a public and several private methods

--- example/config/boot.php
+++ example/config/boot.php
@@ -4,11 +4,9 @@ if(!defined('APPLICATION_ENV')){
   define('APPLICATION_ENV', 'development');
 }
 
-/** Set application base path and load the framework **/
+/** Set application base path **/
 define('FUNKY_APPLICATION_BASE_PATH', realpath(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'));
-require_once(FUNKY_APPLICATION_BASE_PATH.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'funky'.DIRECTORY_SEPARATOR.'framework.php');
 
-/** Database setup **/
-$database = Funky\Config\Json::read(FUNKY_APPLICATION_BASE_PATH.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'database.json');
-Funky\Data\Connection::setup($database[APPLICATION_ENV]);
-$connection = Funky\Data\Connection::getInstance();
+/** Load the framework **/
+require_once(FUNKY_APPLICATION_BASE_PATH.DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'funky'.DIRECTORY_SEPARATOR.'framework.php');
+Funky\Framework::load();
--- lib/framework.php
+++ lib/framework.php
@@ -2,19 +2,75 @@
 namespace Funky;
 
 /**
- * framework loader file
+ * Funky\Framework
  *
- * This fill will load the basic components and settings for the framework.
+ * This class will load the basic components and settings for the Funky Framework.
  */
+class Framework{
+  
+  /**
+   * load
+   *
+   * Call load to load the framework's basic components.
+   */
+  public static function load(){  
+    Framework::checkEnvironmentVariables();
+    
+    Framework::registerAutoloader();
+    Framework::setupDatabase();
+  }
 
-/** Stop working if there is no application base path defined **/
-if(!defined('FUNKY_APPLICATION_BASE_PATH')){
-  throw new Exception('The constant "FUNKY_APPLICATION_BASE_PATH" should be defined before calling the framework. It should contain the path to the application\'s base path.');
+  /**
+   * checkEnvironmentVariables
+   *
+   * Private function that makes sure the necessary environment variables are available. If not, exceptions are thrown to stop further execution.
+   */
+  private static function checkEnvironmentVariables(){
+    /** Stop working if there is no application base path defined **/
+    if(!defined('FUNKY_APPLICATION_BASE_PATH')){
+      throw new \Exception('The constant "FUNKY_APPLICATION_BASE_PATH" should be defined before calling the framework. It should contain the path to the application\'s base path.');
+    }
+
+    /** Stop working if there is no application environment set **/
+    if(!defined('APPLICATION_ENV')){
+      throw new \Exception('The constant "APPLICATION_ENV" should be defined before calling the framework. It should contain the name of the application\'s environment.');
+    }
+
+    /** Define the framework base path **/
+    define('FUNKY_FRAMEWORK_BASE_PATH', realpath(dirname(__FILE__)));
+
+    if(APPLICATION_ENV == 'production'){
+      error_reporting(0);
+      ini_set('display_errors', '0');
+    }
+    else{
+      error_reporting(E_ALL);
+      ini_set('display_errors', '1');
+    }
+  }
+
+  /**
+   * registerAutoloader
+   *
+   * Private function to load and register the autoloader
+   */
+  private static function registerAutoloader(){
+    require_once(FUNKY_FRAMEWORK_BASE_PATH.DIRECTORY_SEPARATOR.'autoloader.php');
+    Autoloader::register();
+  }
+  
+  /**
+   * setupDatabase
+   *
+   * Private function that checks if there is a database.json file in the config folder of the application.
+   * If a file can be found, it is used to setup the database connection.
+   */
+  private static function setupDatabase(){
+    /** See if there is a database.json file in the  **/
+    $database_file = FUNKY_APPLICATION_BASE_PATH.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'database.json';
+    if(file_exists($database_file)){
+        $connections = Config\Json::read($database_file);
+        Data\Connection::setup($connections[APPLICATION_ENV]);
+    }    
+  }
 }
-
-/** Define the framework base path **/
-define('FUNKY_FRAMEWORK_BASE_PATH', realpath(dirname(__FILE__)));
-
-/** Load and register the autoloader **/
-require_once(FUNKY_FRAMEWORK_BASE_PATH.DIRECTORY_SEPARATOR.'autoloader.php');
-Autoloader::register();
